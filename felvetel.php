a34b7f15911c3f43881d449d8dcff51933737458
Bartha Ariana <ariabartha@gmail.com>
2024-01-04 15:16:36 +0100

    Navbar bootstrappel formázva

--- felvetel.php
+++ felvetel.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Felvétel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav>
-        <a href="index.php">Listázás</a>
-        <a href="felvetel.php">Felvétel</a>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark" fixed-top bg-body-tertiary>
+        <div class="container-md">
+            <a class="navbar-brand" href="index.php">Listázás</a>
+            <a class="navbar-brand" href="felvetel.php">Felvétel</a>
+        </div>
     </nav>
+    <main class="container">
+
+    </main>
 </body>
 </html>
