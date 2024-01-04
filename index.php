f7899130843c793c87d0c96282cac8dee4c05c19
Bartha Ariana <ariabartha@gmail.com>
2024-01-04 14:57:44 +0100

    Navigáció létrehozva

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    
+    <nav>
+        <a href="index.php">Listázás</a>
+        <a href="felvetel.php">Felvétel</a>
+    </nav>
+    <main class="container">
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr></tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </main>
 </body>
 </html>
