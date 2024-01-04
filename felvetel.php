6424a01afbc60a064c7d715bd8a5538a51c843ff
Bartha Ariana <ariabartha@gmail.com>
2024-01-04 16:54:51 +0100

    Ellenőrizve az adatok elküldése, új rekord felvétele kész + radio button bekérés javítva

--- felvetel.php
+++ felvetel.php
@@ -33,13 +33,38 @@
                 <input type="number" name="width" id="width" class="form-control">
             </div>
             <div class="mb-3">
+                <label for="wireless" class="form-check-label">Vezetékes</label>
+                <input type="radio" name="wireless" id="wired" value="0" class="form-check-input">
                 <label for="wireless" class="form-check-label">Vezeték nélküli</label>
-                <input type="radio" name="wireOrNot" id="wireless" value="wireless" class="form-check-input">
-                <label for="wired" class="form-check-label">Vezetékes</label>
-                <input type="radio" name="wireOrNot" id="wired" value="wired" class="form-check-input">
+                <input type="radio" name="wireless" id="wireless" value="1" class="form-check-input">
+                
             </div>
             <button type="submit" class="btn btn-outline-success">Elküld</button>
         </form>
     </main>
+    <?php
+    $errors = [];
+    if (!isset($_POST["name"]) || empty($_POST["name"])){
+        $errors[] = "Billentyűzet nevének megadása kötelező";
+    }
+    if (!isset($_POST["type"]) || empty($_POST["type"])){
+        $errors[] = "Billentyűzet típus megadása kötelező";
+    }
+    if (!isset($_POST["layout"]) || empty($_POST["layout"])){
+        $errors[] = "Billenytyűzet lokalizáció megadása kötelező";
+    }
+    if (!isset($_POST["width"]) || empty($_POST["width"])){
+        $errors[] = "Billentyűzet szélességének megadása kötelező";
+    }
+    if (!isset($_POST["wireless"]) || empty($_POST["wireless"])){
+        $errors[] = "Kötelező megadni, hogy vezetékes-e vagy sem.";
+    }
+
+    if (empty($errors)){
+        require_once "KeyboardsTableMethods.php";
+        $database = new KeyboardsTableMethods();
+        $database->create($_POST);
+    }
+    ?>
 </body>
 </html>
