c55e9680f0a437d6a40da6606f67dbae2f11ece0
Bartha Ariana <ariabartha@gmail.com>
2024-01-04 16:26:46 +0100

    adatbeszúráshoz create function létrehozva

--- KeyboardsTableMethods.php
+++ KeyboardsTableMethods.php
@@ -13,5 +13,18 @@ class KeyboardsTableMethods
         $result = $this->connection->query($sql);
         return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    function create($data){
+        $sql = "INSERT INTO keyboards (name,type,layout,width,wireless)
+                    VALUES (?,?,?,?,?)";
+        $stmt = $this->connection->prepare($sql);
+        $name = $data['name'];
+        $type = $data['type'];
+        $layout = $data['layout'];
+        $width = $data['width'];
+        $wireless = $data['wireless'];
+        $stmt->bind_param("sssss", $name, $type, $layout, $width, $wireless);
+        $stmt->execute();
+    }
 }
 ?>
